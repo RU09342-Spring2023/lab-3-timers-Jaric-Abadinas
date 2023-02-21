7a4ac0da007eaddaf1d2cf6a48ec76b7aea390fc
Jaric <abadin48@students.rowan.edu>
2023-02-21 00:18:15 -0500

    Part 2 - Current Progress
    This is my current progress for Part 2. Only shows one speed for the red led blinking. When I add breakpoints between each if statement, it seems to quickly change through the speeds until it gives up and goes back to the default speed (im not sure what is happening). I open the registers and there are moments where either all of P2 or TB1 say "Error value cannot be read".

--- 2. Timers/Part2.c
+++ 2. Timers/Part2.c
@@ -2,7 +2,7 @@
  * Part2.c
  *
  *  Created on: Feb 11, 2023
- *      Author: Russell Trafford
+ *      Author: Jaric Abadinas
  *
  *      This code will need to change the speed of an LED between 3 different speeds by pressing a button.
  */
@@ -29,16 +29,27 @@ void main(){
 
 
 void gpioInit(){
-    // @TODO Initialize the Red or Green LED
+    // @TODO Initialize the Red or Green LED --> doing Red LED
+    P1OUT &= ~BIT0;                         // Clear P1.0 output latch for a defined power-on state
+    P1DIR |= BIT0;                          // Set P1.0 to output direction
 
     // @TODO Initialize Button 2.3
 
+    // P2DIR &= ~BIT3;                       // Configure Pin 2.3 to an input
+    P2REN |= BIT3;                        // Enable the pull up/down resistor for Pin 2.3
+    P2OUT |= BIT3;                        // Pull-up resistor
 
+    // Configure Button on P2.3 as input with pullup resistor
+    P2IES |= BIT3;                          // P2.3 High --> Low edge
+    P2IE |= BIT3;                           // P2.3 interrupt enabled
+    //P2IFG &= ~BIT3;                         // Clear interrupt flag
 }
 
 void timerInit(){
     // @TODO Initialize Timer B1 in Continuous Mode using ACLK as the source CLK with Interrupts turned on
-
+    TB1CTL = TBSSEL_1 | MC_2;                 // ACLK, continuous mode
+    TB1CCTL0 |= CCIE;                         // TBCCR0 interrupt enabled
+    TB1CCR0 = 32768;
 }
 
 
@@ -51,9 +62,24 @@ void timerInit(){
 __interrupt void Port_2(void)
 {
     // @TODO Remember that when you service the GPIO Interrupt, you need to set the interrupt flag to 0.
+    P2IFG &= ~BIT3;                         // Clear P2.3 IFG
 
     // @TODO When the button is pressed, you can change what the CCR0 Register is for the Timer. You will need to track what speed you should be flashing at.
-
+    if (P2IES & BIT3) {
+        if (TB1CCR0 == 32768) {
+            TB1CCR0 = 16384;
+        }
+        else if (TB1CCR0 == 16384) {
+            TB1CCR0 = 8192;
+        }
+        else {
+            TB1CCR0 = 32768;
+        }
+        P2IES &= ~BIT3;
+    }
+    else if (!(P2IES & BIT3)) {
+        P2IES |= BIT3;
+    }
 }
 
 
@@ -62,6 +88,7 @@ __interrupt void Port_2(void)
 __interrupt void Timer1_B0_ISR(void)
 {
     // @TODO You can toggle the LED Pin in this routine and if adjust your count in CCR0.
+    P1OUT ^= BIT0;
 }
 
 
