2cdae01e08bf264acaa4466d2366d444fee33402
Jaric <abadin48@students.rowan.edu>
2023-02-25 01:16:14 -0500

    Part 3 - Current Progress (2)
    Button blinks at 4 Hz on start up. Still need to figure out P2.3 button function on setting button based delay (I feel like I am getting closer). Confirmed that P4.1 works by resetting blinks at 4 Hz.

--- 3. Design Challenge Button Based Delay/Part3.c
+++ 3. Design Challenge Button Based Delay/Part3.c
@@ -9,6 +9,8 @@
 
 #include <msp430.h>
 
+int buttonPressDuration = 0;
+
 void gpioInit();
 void timerInit();
 
@@ -20,36 +22,40 @@ void main() {
 
     PM5CTL0 &= ~LOCKLPM5;
 
-    __bis_SR_register(LPM3_bits | GIE);
+   __bis_SR_register(LPM3_bits | GIE);
 }
 
 
 void gpioInit() {
 
-    P1OUT &= ~BIT0;                         // Clear P1.0 output latch for a defined power-on state
     P1DIR |= BIT0;                          // Set P1.0 to output direction
+    P1OUT &= ~BIT0;                         // Clear P1.0 output latch for a defined power-on state
 
+    P2DIR &= ~BIT3;
     P2REN |= BIT3;                          // Enable the pull up/down resistor for Pin 2.3
     P2OUT |= BIT3;                          // Pull-up resistor
     P2IES |= BIT3;                          // P2.3 High --> Low edge
+    P2IFG &= ~BIT3;
     P2IE |= BIT3;                           // P2.3 interrupt enabled
 
+    P4DIR &= ~BIT1;                         // Config P4.1 as input
     P4REN |= BIT1;                          // Enable the pull up/down resistor for Pin 4.1
     P4OUT |= BIT1;                          // Pull-up resistor
     P4IES |= BIT1;                          // P4.1 High --> Low edge
+    P4IFG &= ~BIT1;                         // Clear P4.1 IRQ Flag
     P4IE |= BIT1;                           // P4.1 interrupt enabled
 }
 
 
 void timerInit() {
-    TB1CTL |= TBCLR;
-    TB1CTL |= TBSSEL_1;
-    TB1CTL |= MC_3;                 // ACLK, continuous mode
-    TB1CCR0 = 8192;
+    TB0CTL |= TBCLR;                    // Clear timer and dividers
+    TB0CTL |= TBSSEL__ACLK;             // Source = ACLK
+    TB0CTL |= MC__UP;                   // Mode = UP;
+    TB0CCR0 = 8192;                     // Timer up to 4 Hz
 
     // Set up Timer Compare IRQ
-    TB1CCTL0 |= CCIE;
-    TB1CCTL0 &= ~CCIFG;
+    TB0CCTL0 |= CCIE;                   // Enable TB0 CCR0 Overflow IRQ
+    TB0CCTL0 &= ~CCIFG;                 // Clear CCR0 Flag
 }
 
 
@@ -57,53 +63,47 @@ void timerInit() {
  * INTERRUPT ROUTINES
  */
 
+
 // Port 2 interrupt service routine
 #pragma vector=PORT2_VECTOR
-__interrupt void Port_2(void)
+__interrupt void ISR_Port2_S2(void)
 {
-    int buttonPressDuration = 0;
-
-    // @TODO Remember that when you service the GPIO Interrupt, you need to set the interrupt flag to 0.
-    P2IFG &= ~BIT3;                         // Clear P2.3 IFG
-
-    // @TODO When the button is pressed, you can change what the CCR0 Register is for the Timer. You will need to track what speed you should be flashing at.
-    if (P2IES & BIT3) {
-        buttonPressDuration = 0;
-        TB1CTL &= ~MC_3;
-        P2IES &= ~BIT3;
-    }
-    else if (!(P2IES & BIT3)) {
-        TB1CCR0 = buttonPressDuration * 32.768;
-        TB1CTL |= TBCLR;
-        TB1CTL |= MC_3;
-    }
+   TB0CTL &= ~MC__UP;          // Stop the timer
+   buttonPressDuration = 0;
+   P1OUT |= BIT0;
+   while (P2IES & BIT3) {
+       __delay_cycles(1000);
+       buttonPressDuration++;
+       P2IES &= ~BIT3;
+   }
+   if (!(P2IES & BIT3)) {
+       P1OUT &= ~BIT0;
+       TB0CCR0 = buttonPressDuration * 32768 / 1000;
+       //TB0CTL |= TBCLR;    // clear the timer
+       TB0CTL |= MC__UP;   // start the timer
+       P2IES |= BIT3;
+   }
+   P2IFG &= ~BIT3;
 }
+
 
 
 // Port 4 interrupt service routine
 #pragma vector=PORT4_VECTOR
-__interrupt void Port_4(void)
+__interrupt void ISR_Port4_S1(void)
 {
-    // @TODO Remember that when you service the GPIO Interrupt, you need to set the interrupt flag to 0.
-    P4IFG &= ~BIT1;                         // Clear P4.1 IFG
-
-    // @TODO When the button is pressed, you can change what the CCR0 Register is for the Timer. You will need to track what speed you should be flashing at.
-    if (P4IES & BIT1) {
-        TB1CCR0 = 8192;
-        P4IES &= ~BIT1;
-    }
+    // TB0CTL |= TBCLR;
+    TB0CCR0 = 8192;
+    P4IFG &= ~BIT1;
 }
 
 
-// Timer B1 interrupt service routine
-#pragma vector = TIMER1_B0_VECTOR
-__interrupt void Timer1_B0_ISR(void)
+// Timer B0 interrupt service routine
+#pragma vector = TIMER0_B0_VECTOR
+__interrupt void ISR_TB0_CCR0(void)
 {
-    // @TODO You can toggle the LED Pin in this routine and if adjust your count in CCR0.
-    P1OUT ^= BIT0;
-    TB1CCTL0 &= ~CCIFG;
+    P1OUT ^= BIT0;                  // Toggle Red LED
+    TB0CCTL0 &= ~CCIFG;             // Clear CCR0 flag
 }
-
-
 
 
