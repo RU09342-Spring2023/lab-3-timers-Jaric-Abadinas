57974280fc85a969f35d3c791c8c19a99168e44a
Jaric <abadin48@students.rowan.edu>
2023-03-19 20:20:56 -0400

    Part 3 - Finished
    The button delay works however it is not exactly hold for 2 seconds it will blink 2 seconds. If you hold long it takes longer to blink though.

--- 3. Design Challenge Button Based Delay/Part3.c
+++ 3. Design Challenge Button Based Delay/Part3.c
@@ -48,12 +48,14 @@ void gpioInit() {
 
 
 void timerInit() {
+    TB1CCR0 = 300;  //arbitrary initial value
+    TB1CTL |= TBSSEL__ACLK + MC_0 + TBCLR; // Set the clock source to ACLK, stop the timer, and clear it
+    TB1CCTL0 |= CAP + CM_3; // Set Timer B0 to capture mode, set for both edges
+
     TB0CTL |= TBCLR;                    // Clear timer and dividers
     TB0CTL |= TBSSEL__ACLK;             // Source = ACLK
     TB0CTL |= MC__UP;                   // Mode = UP;
     TB0CCR0 = 8192;                     // Timer up to 4 Hz
-
-    // Set up Timer Compare IRQ
     TB0CCTL0 |= CCIE;                   // Enable TB0 CCR0 Overflow IRQ
     TB0CCTL0 &= ~CCIFG;                 // Clear CCR0 Flag
 }
@@ -68,22 +70,22 @@ void timerInit() {
 #pragma vector=PORT2_VECTOR
 __interrupt void ISR_Port2_S2(void)
 {
-   TB0CTL &= ~MC__UP;          // Stop the timer
-   buttonPressDuration = 0;
-   P1OUT |= BIT0;
-   while (P2IES & BIT3) {
-       __delay_cycles(1000);
-       buttonPressDuration++;
+   if (P2IES & BIT3) {
+       TB1CTL |= MC_2;
        P2IES &= ~BIT3;
    }
-   if (!(P2IES & BIT3)) {
-       P1OUT &= ~BIT0;
-       TB0CCR0 = buttonPressDuration * 32768 / 1000;
-       //TB0CTL |= TBCLR;    // clear the timer
-       TB0CTL |= MC__UP;   // start the timer
+
+   else if (P2IES != BIT3) {
+       TB1CTL &= ~(MC0 + MC1); //stop timer B0 to record how long the button was pressed
+
+       //Put timer B0 value into the max timer B1 CCR0
+       buttonPressDuration = TB1R; // Record the button press time
+       TB0CCR0 = buttonPressDuration; // Set the max count for Timer B1 to the button press time
+
+       TB1CTL |= TBCLR; //clear timer B0 for next button press
        P2IES |= BIT3;
    }
-   P2IFG &= ~BIT3;
+   P2IFG &= ~BIT3;         // Clear interrupt flag for P2.3
 }
 
 
@@ -92,7 +94,8 @@ __interrupt void ISR_Port2_S2(void)
 #pragma vector=PORT4_VECTOR
 __interrupt void ISR_Port4_S1(void)
 {
-    // TB0CTL |= TBCLR;
+    TB1CTL &= ~(MC0 + MC1); // Stop Timer B0 - just in case
+    TB1CTL |= TBCLR; // Clear Timer B0
     TB0CCR0 = 8192;
     P4IFG &= ~BIT1;
 }
